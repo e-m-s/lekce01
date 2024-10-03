43d9e064100989e654461a1cfa849e3c1efe8755
Martin Simunek <public@e-mis.cz>
2024-10-03 18:16:59 +0200

    pridej ukazku trid a objektu
    Vytvoreny tridy Classmate a School.
    V metode main vytvoren prvni objekt.

--- src/Classmate.java
+++ src/Classmate.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
+import java.time.LocalDate;
 
 public class Classmate {
+    private String name;
+    private LocalDate birthday;
+    private int grade;
+    private School school;
+
 }
--- src/School.java
+++ src/School.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
-
 public class School {
+    private String name;
+    private String address;
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
